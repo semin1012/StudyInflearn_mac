99dead5699ae63703df24322a1c787bd40f1780e
semin1012 <semin1012@gmail.com>
2024-05-01 21:53:45 +0900

    Initial Commit

---
+++ GameCoding/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GameCoding
+//
+//  Created by 이세민 on 5/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

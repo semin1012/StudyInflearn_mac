3c49d71bb32b2fe4d72a482e978d88814db1d5ff
semin1012 <semin1012@gmail.com>
2024-05-14 21:01:38 +0900

    8주차 함수 포인터에 대하여
    함수 포인터의 사용법과 어떨 때 유용하게 사용하는지 등을 알아봤다.

--- GameCoding/main.cpp
+++ GameCoding/main.cpp
@@ -3,186 +3,129 @@
 #include <queue>
 using namespace std;
 
-// DFS(depth first search): 깊이 들어가며 정보를 파악하는 것, 깊이 우선
-// DFS = 재귀함수 = 스택
-
-// BFS (breath first search): 너비 우선 탐색.
-// BFS = 큐 (먼저 접근한 것은 먼저 처리)
-
-struct Vertex
+void Print()
 {
-    // int data;
+    cout << "Hello World" << endl;
+}
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+// 콜백 함수
+// - ex) UI
+// - ex) 온라인 게임
+// - ex) 키보드 입력
+
+using FuncPtrType = int(*)(int a, int b);
+int DoSometing(int a, int b, FuncPtrType func)
+{
+    return func(a, b);
+}
+
+void Fire()
+{
+    cout << "공격" << endl;
+}
+
+class Item
+{
+public:
+    
+public:
+    int _itemId = 0;
+    int _rarity = 0;
+    int _ownerId = 0;
 };
 
-vector<Vertex> vertices;
-vector<vector<int>> adjacent;
-
-// 내가 방문한 목록 -> DFS일 때
-vector<bool> visited;
-
-// 내가 발견한 목록 -> BFS일 때
-vector<bool> discovered;
-
-void CreateGraph()
+using ItemSelectorType = bool(*)(Item* item);
+Item* FindItem(Item items[], int itemCount, ItemSelectorType selector)
 {
-    vertices.resize(6);
-    
-    // 인접 리스트
-    adjacent = vector<vector<int>>(6);
-    adjacent[0] = { 1, 3 };
-    adjacent[1] = { 0, 2, 3 };
-    adjacent[3] = { 4 };
-    adjacent[5] = { 4 };
-    
-    
-    // 인접 행렬
-//    adjacent = vector<vector<int>>
-//    {
-//        {0, 1, 0, 1, 0, 0},
-//        {1, 0, 1, 1, 0, 0},
-//        {0, 0, 0, 0, 0, 0},
-//        {0, 0, 0, 0, 1, 0},
-//        {0, 0, 0, 0, 0, 0},
-//        {0, 0, 0, 0, 1, 0}
-//    };
-}
-
-void Dfs(int here)
-{
-    // 방문 도작
-    visited[here] = true;
-    cout << "Visited : " << here << endl;
-    
-    // 인접한 길을 다 체크해서
-// 이건 방향 그래프일 때, 인접리스트일 때 사용
-    // E(edge) = 총 개수
-    // 인접리스트 -> O(V+E). E가 V 개수 그대로라면 결국 V^2이 될 수 있음.
-//    const int size = adjacent[here].size();
-//    for ( int i = 0 ; i < size; i++ )
-//    {
-//        int there = adjacent[here][i];
-//        if ( visited[there] == false )
-//            Dfs(there);
-//    }
-    
-    
-    // 인접 행렬 -> O(V^2) = 정점마다 한 번씩 + for문을 V마다 한 번씩 하기 때문. 행렬 ㅇㅇ
-    for ( int there = 0; there < 6; there++)
+    for ( int i = 0 ; i < itemCount ; i++ )
     {
-        // 길은 있는지
-        if (adjacent[here][there] == 0)
-            continue;
-        
-        // 아직 방문하지 않은 곳에 한해서 방문
-        if ( visited[there] == false )
-            Dfs(there);
-        // 이렇게 하면 5번은 안 들리고 끝남
-        // 4번에서 5번으로 가는 길 자체가 없기 때문에. 그래프의 문제임
-        // 그래서 DfsAll()을 따로 만들어서 visited를 다 돌도록 만들어야 한다.
+        Item* item = &items[i];
+        if (selector(item))
+            return item;
     }
+    
+    return nullptr;
 }
 
-// 빽빽할 때는 인접 행렬 쓰면 되고,
-// 느슨하다면 인접 리스트를 써야 훨씬 빠름.
-
-void DfsAll()
+bool IsRare(Item* item)
 {
-    visited = vector<bool>(6, false);
-    
-    for (int i = 0 ; i < 6; i++ )
-        if (visited[i] == false )
-        {
-            Dfs(i);
-            cout << "Hi" << endl;   // 두 번 호출되고 visited 다 돌아서 끝남
-        }
-}
-
-void Bfs(int here)
-{
-    // 아무 곳이나 들어가는 방식
-    // 입구에서 얼마나 떨어져 있는지. 가까이 있는 곳부터 접근을 하겠다.
-    // 쉽게 말해서 발견한 순서대로 접근하면 되는 것이다.
-    
-    // ex) 누구에 의해서 발견되었는지?
-    vector<int> parent(6, -1);
-    
-    // ex) 시작점에서 얼만큼 떨어져 있는지?
-    vector<int> dist(6, -1);
-    
-    queue<int> q;
-    q.push(here);   // 예약 시스템
-    discovered[here] = true;  // 발견한 곳인지
-    // ex)에 관한 변수
-    parent[here] = here;
-    dist[here] = 0; // 거리
-    
-    
-    while (q.empty() == false) // 새롭게 지점을 발견하지 못 할 때까지
-    {
-        // 처음에는 넣자마자 빼는 것처럼 보이지만 다음 턴에서는 새롭게 발견된 애들이 추가됨
-        here = q.front();
-        q.pop();
-        
-        // 방문 도장
-        cout << "Visited: " << here << endl;
-        
-        // 인접한 애들 찾아주기, 인접 리스트로 만들었을 경우의 코드!
-        int size = adjacent[here].size();
-        for ( int i = 0; i < size; i++ )
-        {
-            int there = adjacent[here][i];
-            if ( discovered[there]) // 이미 다른 경로를 통해 발견한 곳이라면
-                continue;   // 넘어가기
-            
-            // 새롭게 발견된 곳이라면 예약해 주기
-            q.push(there);
-            // q에 새롭게 추가되었기 때문에 while문이 안 끝나고 더 돌게 됨
-            discovered[there] = true;
-            
-            // ex)에 관한 코드
-            parent[there] = here;
-            // 여기까지 접근하는 데 here만큼 걸린 것에 +1을 하는 것
-            dist[there] = dist[here] +1;
-            
-            cout << "there: " << there << "  parent: " << parent[there] << "  dist: " << dist[there] << endl;
-        }
-        
-        // 인접 행렬일 경우의 코드!
-//        for ( int there = 0; there < 6; there++ )
-//        {
-//            if ( adjacent[here][there] == 0 ) // 값 존재하지 않는다면
-//                continue;
-//            if ( discovered[there] == true )
-//                continue;
-//            // 밑에 부분은 인접리스트나, 인접행렬이나 똑같음
-//            q.push(there);
-//            discovered[there] = true;
-//        }
-    }
-}
-
-void BfsAll()
-{
-    discovered = vector<bool>(6, false);
-    
-    for ( int i = 0 ; i < 6; i++ )
-        if (discovered[i] == false )
-            Bfs(i);
-        // 아직 못 찾은 숫자의 너비가 있다면 (0~5까지의 숫자 존재하기 때문에 6 숫자 넣음)
-        // Bfs(i) 함수 실행
+    return item->_rarity == 1;
 }
 
 int main()
 {
-    CreateGraph();
+    // 함수 객체, 함수 펑션이 greater, less다.
+    //priority_queue<int, vector<int>, greater<int>> pq;
     
-    discovered = vector<bool>(6, false);
+    int a = 10;
+    //typedef int DataType;
+    using DataType = int;   // 위와 같은 뜻
+    // DataType* ptr = &a;
     
-    BfsAll();
-    // 너비 우선이기 때문에 가까운 애들부터 탐색한다는 특징이 있다.
-    // Bfs랑 길찾기랑 무슨 상관이 있는가?
-    // Bfs의 순서가 최단거리라는 것이 보장이 되는가?
-    // -> Bfs로 찾은 최단거리보다 짧은 거리 찾을 수 없음. 순차적으로 몇 번째로 먼 건지 보기 때문에 더 빠른 경로가 나올 수 없다. 먼저 찾아지는 경로가 최단 거리임 ㅇㅇ. 따라서 최단경로를 찾음.
-    // 대신 Dfs는 처음에 찾아지는 경로가 최단 거리인 건 아니지만, 검색 대상의 그래프가 정말 크다면 Dfs를 고려한다. 또한 경로에 저장해야 할 특징이 있을 때 사용한다.
+    // 포인터: 이것을 타고 가면 무엇이 있는지 주장하는 것
+    // 함수도 포인터를 들고 있을 수 있다는 것이 오늘의 주제다.
+    
+    //typedef void FuncType();      // << 옛날 방식
+    using FuncType = void();    // << 현대 방식
+    // 함수는 함수인데 보이드를 뱉어내고 보이드를 받아주는 FuncType이라는 뜻
+    
+    FuncType* ptr = &Print;     // FuncType에 함수 포인터를 넣을 수가 있게 된다
+    
+    ptr();
+    // 함수를 막 호출하는 것이 아니라 함수의 주소를 가지고 부른다는 의미다.
+    
+    //-----------------------------------------------------------------
+    
+    // ptr = &add; // << 이건 안 된다.
+    // 왜냐하면 FuncType형은 void를 받아 void를 뱉어나는 사용자 자료형인데 형이 다름
+    
+    using FuncType2 = int(int, int);
+    FuncType2* ptr2 = &Add;
+    
+    ptr2(10, 20);   // << 이제 형식이 맞아서 가능
+    
+    //-----------------------------------------------------------------
+    // 사실 위의 함수 포인터들 정말 안 씀. 있는 줄도 모르는 사람 많다
+    // 그래서 밑에처럼 쓴다.
+    
+    // void(*FuncPtrType)();        // << 옛날 문법
+    //using FuncPtrType = void(*)();  // << 현대 문법
+    // 보이드를 받고, 보이드를 뱉는 함수 포인터라는 뜻
+    
+    //FuncPtrType ptr3 = &Print;
+    //ptr3();
+    
+    
+    
+    
+    
+    // !!!!!! 대체 왜 이 짓거리를 하는가? 어떤 의미길래? !!!!!!
+    // 생각보다 유용할 때가 많다.
+    // - 콜백, 비동기 상황 등
+    // '행동' 자체를 인자로 넘기고 싶을 때 << DoSomething과 같이 만든다
+    DoSometing(10, 20, &Add);
+    DoSometing(10, 20, [](int a, int b) { return a - b;});
+    // 이처럼 정말 다양하게 사용할 수가 있다.
+    
+    
+    // 키보드 조작 시 사용
+    using OnClickKeyboard = void(*)();
+    OnClickKeyboard gSkill = &Fire;
+    // if ( Q버튼을 눌렀으면)
+    //  gSkill()
+    
+    
+    // 인벤토리 상황
+    Item items[10];
+    items[3]._rarity = 1;   // RARE
+    
+    // IsRare 같은 함수를 조건으로 넣을 수가 있다. 다양한 조건 넣어서 만들 수 있다. 
+    FindItem(items, 10, IsRare);
     
 }
+
---
+++ GameCoding_RPG/GameCoding_RPG/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GameCoding_RPG
+//
+//  Created by 이세민 on 5/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
